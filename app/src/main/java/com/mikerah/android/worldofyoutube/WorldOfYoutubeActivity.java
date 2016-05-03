9500bc87a92859a3c8304e73255aabbb7002c641
Mikerah <Mikerah1@users.nonreply.github.com>
2016-05-03 20:02:32 -0300

    Adding default android studio files

---
+++ app/src/main/java/com/mikerah/android/worldofyoutube/WorldOfYoutubeActivity.java
@@ -0,0 +1,13 @@
+package com.mikerah.android.worldofyoutube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorldOfYoutubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world_of_youtube);
+    }
+}
